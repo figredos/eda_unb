6e5a2c8583320b09fd19b40a87789ba3869d55ba
figredos <matheusmfellet@gmail.com>
2023-10-06 13:51:45 -0300

    Exercicio rumo9s update

--- exercicios_moj/lista_1/rumo9s.c
+++ exercicios_moj/lista_1/rumo9s.c
@@ -1,33 +1,12 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// void strrev(char *str)
-// {
-//     // if the string is empty
-//     if (!str)
-//     {
-//         return;
-//     }
-//     // pointer to start and end at the string
-//     int i = 0;
-//     int j = strlen(str) - 1;
-
-//     // reversing string
-//     while (i < j)
-//     {
-//         char c = str[i];
-//         str[i] = str[j];
-//         str[j] = c;
-//         i++;
-//         j--;
-//     }
-// }
-
 void rumo_9(char numero[], int *multiplo, int *profundidade)
 {
     int soma = 0;
-    char *soma_final = malloc(sizeof(char) * 9000);
+    char *soma_final = calloc(sizeof(char), strlen(numero));
 
     for (int i = 0; i < strlen(numero); i++)
     {
@@ -42,14 +21,12 @@ void rumo_9(char numero[], int *multiplo, int *profundidade)
             soma = (soma - (soma % 10)) / 10;
         }
 
-        // strrev(soma_final);
-
         (*profundidade)++;
 
         rumo_9(soma_final, multiplo, profundidade);
     }
     else
-    {
+    {   
         if ((soma % 9) == 0)
             *multiplo = 1;
     }
@@ -57,13 +34,16 @@ void rumo_9(char numero[], int *multiplo, int *profundidade)
 
 int main()
 {
-    char *numero = malloc(sizeof(char) * 9000);
+    char *numero = malloc(sizeof(char) * 1000);
 
     int multiplo = 0, profundidade = 1;
 
     do
     {
-        scanf("%s", numero);
+        fgets(numero, 1000, stdin);
+
+        numero[strlen(numero) - 1] = '\0';
+
         if (numero[0] != '0')
         {
             char *numero_novo = numero;
@@ -83,19 +63,3 @@ int main()
 
     return 0;
 }
-
-// int main()
-// {
-//     char *numero = "999999";
-//     // scanf("%s", numero) malloc(sizeof(char) * 9000);
-
-//     char *numero_novo = numero;
-
-//     int multiplo = 0, profundidade = 0;
-
-//     rumo_9(numero_novo, &multiplo, &profundidade);
-
-//     printf("%s %d %d\n", numero_novo, multiplo, profundidade);
-
-//     return 0;
-// }
