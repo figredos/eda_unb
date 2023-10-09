16c226fd2f339f4ea845c324d58ceccb71be2740
figredos <matheusmfellet@gmail.com>
2023-10-09 17:10:37 -0300

    Exercício maior_substring incompleto

--- exercicios_moj/lista_1/maior_substring.c
+++ exercicios_moj/lista_1/maior_substring.c
@@ -2,50 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-void maior_substring(char *string, char *substring, int indice, int *tamanho)
+void maior_substring(char *string, char *substring, int *indice, int *tamanho, int *flag)
 {
-    if (indice < strlen(string))
+    if ((*indice) < strlen(string))
     {
-        if (!strncmp(string + indice, substring, strlen(substring)))
+        if (!strncmp(string + *indice, substring, strlen(substring)))
         {
-            indice += strlen(substring);
+            (*indice) += strlen(substring);
+
+            if (*tamanho == 0)
+                *flag = 1;
+            else
+                *flag = 0;
 
             (*tamanho) += strlen(substring);
 
-            maior_substring(string, substring, indice, tamanho);
+            maior_substring(string, substring, indice, tamanho, flag);
         }
-        indice++;
-        maior_substring(string, substring, indice, tamanho);
+        if (*flag > 0)
+            (*tamanho)++;
+        (*indice)++;
+        maior_substring(string, substring, indice, tamanho, flag);
     }
-}
-
-int main()
-{
-    char *string = "catcowcat";
-    char *substring = "cat";
-    int tamanho = 0;
-
-    maior_substring(string, substring, 0, &tamanho);
-
-    printf("%d\n", tamanho);
-
-    return 0;
 }
 
 // int main()
 // {
-//     char *string = malloc(sizeof(char) * 100);
-//     char *substring = malloc(sizeof(char) * 100);
+//     char *string = "catcowcat";
+//     char *substring = "cat";
+//     int indice = 0;
 //     int tamanho = 0;
+//     int flag = 0;
 
-//     scanf("%s", string);
-//     scanf("%s", substring);
-
-//     maior_substring(string, substring, 0, &tamanho, 0);
+//     maior_substring(string, substring, &indice, &tamanho, &flag);
 
 //     printf("%d\n", tamanho);
 
 //     return 0;
 // }
+
+int main()
+{
+    char *string = malloc(sizeof(char) * 100);
+    char *substring = malloc(sizeof(char) * 100);
+    int indice = 0;
+    int tamanho = 0;
+    int flag = 0;
+
+    scanf("%s", string);
+    scanf("%s", substring);
+
+    maior_substring(string, substring, &indice, &tamanho, &flag);
+
+    printf("%d\n", tamanho);
+
+    return 0;
+}
 
 // Tem que checar se o tamanho achado é o maior
