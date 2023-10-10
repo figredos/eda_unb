09f9f140ee3247b7534d0c552c73aa7eae6ab717
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-10-10 17:55:05 -0300

    Exercício ultrapassa_minimamente

--- exercicios_moj/lista_1/ultrapassa_minimamente.c
+++ exercicios_moj/lista_1/ultrapassa_minimamente.c
@@ -1,60 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// entrada: 33 51 23 94 66 28 11 73 19 8 31 0
-// saida: 19 28 94 23
-
 void ultrapassa_minimamente(int *numeros, int limite, int indice, int tamanho)
 {
     if ((indice) < tamanho)
     {
-        int soma = numeros[indice];
+        int soma = 0;
 
-        if (soma > limite)
+        while (soma <= limite && indice < tamanho)
         {
-            indice++;
-            soma = 0;
-        }
-
-        while (soma <= limite)
-        {
-            if (indice < tamanho)
-            {
-                (indice)++;
                 soma += numeros[indice];
-            }
+                (indice)++;
         }
 
         ultrapassa_minimamente(numeros, limite, indice, tamanho);
 
         if ((indice) < tamanho)
-            printf("%d\n", numeros[indice]);
+        {
+            printf("%d\n", numeros[indice - 1]);
     }
 }
-
-// int main()
-// {
-//     int *numeros = malloc(100 * sizeof(int));
-
-//     for (int i = 0; numeros[i] != 0; i++)
-//     {
-//         scanf("%d ", numeros);
-//     }
-
-//     ultrapassa_minimamente(numeros, 90, 0);
-
-//     return 0;
-// }
+}
 
 int main()
 {
-    int numeros[12] = {33, 51, 23, 94, 66, 28, 11, 73, 19, 8, 31, 0};
-    int limite = 90;
-    int tamanho = sizeof(numeros) / sizeof(numeros[0]);
-    int indice = 0;
+    int *numeros = NULL;
+    int numero = 1;
+    int capacidade = 0;
+    int limite, indice = 0;
 
-    // printf("%d", sizeof(numeros)/sizeof(numeros[0]));
-    ultrapassa_minimamente(numeros, limite, indice, tamanho);
+    while (numero != 0)
+    {
+        capacidade++;
+        scanf("%d", &numero);
+        numeros = realloc(numeros, capacidade * sizeof(int));
+        numeros[capacidade - 1] = numero;
+    }
+
+    scanf("%d", &limite);
+
+    ultrapassa_minimamente(numeros, limite, indice, capacidade);
 
     return 0;
 }
