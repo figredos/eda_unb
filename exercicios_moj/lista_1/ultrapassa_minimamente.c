8e56a9a0d3cddc6fdaedf71af87cdb44263c7b46
figredos <matheusmfellet@gmail.com>
2023-10-10 14:59:49 -0300

    Exercício ultrapassa_minimamente incompleto

--- exercicios_moj/lista_1/ultrapassa_minimamente.c
+++ exercicios_moj/lista_1/ultrapassa_minimamente.c
@@ -1,36 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ultrapassa_minimamente(int *numeros, int limite, int indice)
+void ultrapassa_minimamente(int *numeros, int limite, int *indice, int tamanho)
 {
-    int soma = 0;
-
-    do
+    if ((*indice) < tamanho)
     {
-        if (numeros[indice] != 0)
+        int soma = 0;
+
+        do
         {
-            soma += numeros[indice];
-            indice++;
-        }
-    } while (soma <= limite);
+            if (numeros[*indice] != 0)
+            {
+                soma += numeros[*indice];
+                (*indice)++;
+            }
+        } while (soma <= limite);
 
-    ultrapassa_minimamente(numeros, limite, indice);
+        (*indice)--;
 
-    printf("%d\n", numeros[indice]);
+        ultrapassa_minimamente(numeros, limite, indice, tamanho);
 
-    return indice;
+        printf("%d\n", numeros[*indice]);
+    }
 }
+
+// void ultrapassa_minimamente(int *numeros, int limite, int indice, int tamanho)
+// {
+
+//     if (indice < tamanho)
+//     {
+//         int soma = 0;
+
+//         for (int i = 0; soma <= limite; i++)
+//         {
+//             soma += numeros[indice];
+//             indice++;
+//         }
+
+//         ultrapassa_minimamente(numeros, limite, indice - 1, tamanho);
+
+//         printf("%d\n", numeros[indice - 1]);
+//     }
+// }
+
+// int main()
+// {
+//     int *numeros = malloc(100 * sizeof(int));
+
+//     for (int i = 0; numeros[i] != 0; i++)
+//     {
+//         scanf("%d ", numeros);
+//     }
+
+//     ultrapassa_minimamente(numeros, 90, 0);
+
+//     return 0;
+// }
 
 int main()
 {
-    int *numeros = malloc(100 * sizeof(int));
+    int numeros[12] = {33, 51, 23, 94, 66, 28, 11, 73, 19, 8, 31, 0};
+    int limite = 90;
+    int tamanho = sizeof(numeros) / sizeof(numeros[0]);
+    int indice = 0;
 
-    for (int i = 0; numeros[i] != 0; i++)
-    {
-        scanf("%d ", numeros);
-    }
-    
-    ultrapassa_minimamente(numeros, 90, 0);
+    // printf("%d", sizeof(numeros)/sizeof(numeros[0]));
+    ultrapassa_minimamente(numeros, limite, &indice, tamanho);
 
     return 0;
 }
