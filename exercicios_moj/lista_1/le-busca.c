cf0b673f9b08c2d0159f5f92b180432dc2276617
figredos <matheusmfellet@gmail.com>
2023-10-18 13:01:21 -0300

    Exercício le-busca

---
+++ exercicios_moj/lista_1/le-busca.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula
+{
+    int dado;
+    struct celula *prox;
+} celula; 
+
+celula *busca(celula *le, int x)
+{
+    celula *temp = le;
+    while (temp != NULL && temp->dado !=x)            
+        temp = temp->prox;
+    return temp;
+}
+
+
+celula *busca_rec(celula *le, int x)
+{
+    if (le == NULL)
+        return NULL;
+    if (le->dado == x)
+        return le;
+    return busca_rec(le->prox, x);
+}
