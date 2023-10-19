3b03c03f18a3fc12a61f8db3a4da7c0bd65db34a
figredos <matheusmfellet@gmail.com>
2023-10-19 14:21:03 -0300

    Exercício f91 comentado

--- exercicios_moj/lista_1/f91.c
+++ exercicios_moj/lista_1/f91.c
@@ -1,20 +1,22 @@
+//Inclui pacotes
 #include <stdio.h>
 #include <stdlib.h>
 
+//Cria função f91
 int f91(int numero)
 {
-    if (numero <= 100)
+    if (numero <= 100)  //Se o número for maior ou igual que 100, faz-se a recursão da função do número + 11
         return f91(f91(numero + 11));
 
-    else
+    else    //Caso o número seja menor que 100, retornamos o número - 10
         return numero - 10;
 }
 
 int main()
 {
-    int numero = 1;
+    int numero = 1; //Inicializando a função
 
-    do
+    do  // Enquanto o número dado como input for diferente de 0, faz a chamada da função e imprime o resultado
     {
         scanf("%d", &numero);
         if (numero != 0)
