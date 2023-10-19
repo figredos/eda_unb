1b3883ed3883dea415f0bbed4120da2a54dfabc4
figredos <matheusmfellet@gmail.com>
2023-10-19 14:21:52 -0300

    Exercício le-divide comentado

--- exercicios_moj/lista_1/le-divide.c
+++ exercicios_moj/lista_1/le-divide.c
@@ -1,26 +1,34 @@
+//Incluindo pacotes
 #include <stdio.h>
 #include <stdlib.h>
 
+//Definindo struct celula
 typedef struct celula
 {
     int dado;
     struct celula *prox;
 } celula;   
 
-void divide_lista(celula *l, celula *l1, celula *l2) {
+//Função que divide uma lista l em duas listas, l1 ímpar, l2 par 
+void divide_lista(celula *l, celula *l1, celula *l2)
+{
+    //Cria células temporárias
     celula *atual = l->prox;
     celula *prev_l1 = l1;
     celula *prev_l2 = l2;
-
+ 
     while (atual != NULL) {
         // Salva o próximo nó antes de movê-lo
         celula *proximo = atual->prox;
 
-        if (atual->dado % 2 == 0) {  // Número par
+        if (atual->dado % 2 == 0)
+        {  // Número par
             prev_l2->prox = atual;
             prev_l2 = atual;
             prev_l2->prox = NULL;
-        } else {  // Número ímpar
+        }
+        else
+        {  // Número ímpar
             prev_l1->prox = atual;
             prev_l1 = atual;
             prev_l1->prox = NULL;
@@ -35,31 +43,12 @@ void divide_lista(celula *l, celula *l1, celula *l2) {
     prev_l2->prox = NULL;
 }
 
-
-// void divide_lista(celula *l, celula *l1, celula *l2)
-// {
-//     celula *impar_ant = l1, *impar = l1->prox;
-//     celula *par_ant = l2, *par = l2->prox;
-
-//     while (l != NULL)
-//     {
-//         if (l->info % 2)
-//         {
-//             celula *salva = l->prox;
-//             impar_ant = l;
-//             impar = impar_ant->prox;
-//             impar_ant = impar;
-//             impar = NULL;
-//             l = salva;
-//         }
-//         else
-//         {  
-//             celula *salva = l->prox;
-//             par_ant = l;
-//             par = par_ant->prox;
-//             par_ant = par;
-//             par = NULL;
-//             l = salva;
-//         }
-//     }
-// }
+/*
+O processo de mudança do ponteiro é o seguinte
+1- Salvamos o próximo número na célula "proximo"
+2- Mudamos o proximo das listas para a célula principal
+3- Mudamos as listas para a célula principal
+4- Mudamos o próximo das listas para nulo, os reiniciando 
+5- Mudamos o valor da célula atual para o ponteiro "proximo"
+6- Ao final do loop while mudar o valor do final das listas para NULL
+*/
