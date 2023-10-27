ca5a6d95da2466d025242d96a54a5e92ec4f2b61
figredos <matheusmfellet@gmail.com>
2023-10-27 14:20:21 -0300

    Exercício filas_lista

--- exercicios_moj/lista_2/fila_lista.c
+++ exercicios_moj/lista_2/fila_lista.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct celula {
+typedef struct celula
+{
     int dado;
     struct celula *prox;
 } celula;
 
-void enfileira(celula **f, int x) {
+void enfileira(celula **f, int x)
+{
     celula *nova_celula = malloc(sizeof(celula));
+    celula *temp = malloc(sizeof(celula));
+    temp = *f;
     nova_celula->dado = x;
-    
-    if (*f == NULL) {
-        nova_celula->prox = nova_celula; 
-        *f = nova_celula; 
-    } else {
-        nova_celula->prox = (*f)->prox; 
-        (*f)->prox = nova_celula; 
-        *f = nova_celula; 
+
+    if(temp != NULL)
+    {
+        while (temp->prox != temp)
+            temp = temp->prox;
+
+        temp->prox = nova_celula;
+        nova_celula->prox = *f;
     }
+    else    
+        *f = nova_celula;
 }
 
-int desenfileira(celula *f, int *y) {
-    if (f == NULL) {
-        return 0; 
-    }
-    
+int desenfileira(celula *f, int *y)
+{
     celula *celula_removida = f->prox;
     *y = celula_removida->dado;
-    
-    if (f->prox == f) {
-        free(f);
-        f = NULL; 
-    } else {
+
+    if (f->prox == f)
+        return 0;
+    else
         f->prox = celula_removida->prox;
-        free(celula_removida);
-    }
-    
+
     return 1;
 }
