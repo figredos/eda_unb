5539f96db4b26f95e5c94d1f72de676a37ea8f20
figredos <matheusmfellet@gmail.com>
2023-11-01 18:03:53 -0300

    Exercicio arvore_binaria_pos

--- exercicios_moj/lista_2/arvore_binaria_pos.c
+++ exercicios_moj/lista_2/arvore_binaria_pos.c
@@ -6,61 +6,39 @@ typedef struct no
     struct no *esq, *dir;
 } no;
 
-// void pos_ordem(no *raiz)
-// {
-//     if (raiz == NULL)
-//         return;
-
-//     no *atual = raiz;
-//     no *pilha[100];
-//     int topo = -1;
-
-//     while (1)
-//     {
-//         while (atual != NULL)
-//         {
-//             pilha[++topo] = atual;
-//             atual = atual->esq;
-//         }
-
-//         if(topo >= -1)
-//         {
-//             atual = pilha[topo--];
-//             atual = atual->dir;
-//             printf("%d ", atual->dado);
-//         }
-//         else
-//             break;
-//     }
-// }
-
-void pos_ordem(no* raiz) {
-    if (raiz == NULL)
+void pos_ordem(no *raiz)
+{
+    if(raiz == NULL)
         return;
 
-    no* atual = raiz;
-    no* pilha1[100]; // Tamanho arbitrário da pilha
-    no* pilha2[100]; // Tamanho arbitrário da pilha
-    int topo1 = -1;  // Inicializa o topo da pilha 1
-    int topo2 = -1;  // Inicializa o topo da pilha 2
+    no *atual = raiz;
+    no *pilha_1[100];
+    no *pilha_2[100];   //empilha para impressão
+    int topo_1 = -1;
+    int topo_2 = -1;
 
-    pilha1[++topo1] = atual;
+    pilha_1[++topo_1] = atual;
 
-    while (topo1 >= 0) {
-        atual = pilha1[topo1--];
-        pilha2[++topo2] = atual;
+    while(topo_1 >=0)
+    {
+        atual = pilha_1[topo_1--];
+        pilha_2[++topo_2] = atual;
 
-        if (atual->esq != NULL)
-            pilha1[++topo1] = atual->esq;
-        if (atual->dir != NULL)
-            pilha1[++topo1] = atual->dir;
+        if(atual->esq != NULL)
+            pilha_1[++topo_1] = atual->esq;
+        if(atual->dir != NULL)
+            pilha_1[++topo_1] = atual->dir;
     }
 
-    while (topo2 >= 0) {
-        atual = pilha2[topo2--];
+    while (topo_2 >= 0)
+    {
+        atual = pilha_2[topo_2--];
         printf("%d ", atual->dado);
     }
+    printf("\n");
+
 }
 
-
-// visitar todos os nós filhos antes de visitar o nó pai
+// visitar visitar os nós da esquerda e da direita antes de visitar o nó pai
+// precisa imprimir antes o nó esq e em seguida o nó dir 
+// para mais informações ver arquivo .\tipos_abstratos_de_dados\arvores_binarias.md
