f4145a4306eaa6b8abcc0daa92fc3cdd63a512f5
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-11-06 14:19:05 -0300

    Exercício elevator_trouble

--- exercicios_moj/lista_2/elevator_trouble.c
+++ exercicios_moj/lista_2/elevator_trouble.c
@@ -1,71 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int minButtonPresses(int f, int s, int g, int u, int d) {
-    int *visited = (int *)malloc((f + 1) * sizeof(int));
-    for (int i = 1; i <= f; i++) {
-        visited[i] = 0;
-    }
+int min_apertar(int f, int s, int g, int u, int d)
+{
+    int *visitados = malloc(sizeof(int) * (f + 1));
 
-    int *queue = (int *)malloc((f + 1) * sizeof(int));
-    int front = 0;
-    int rear = 0;
+    for (int i = 1; i <= f; i++)
+        visitados[i] = 0;
 
-    queue[rear++] = s;
-    visited[s] = 1;
+    int *fila = malloc(sizeof(int) * (f + 1));
+    int inicio = 0;
+    int fim = 0;
 
-    int pushes = 0; // Initialize to 0
-    int levelSize = 1; // Number of elements at the current level
+    fila[fim++] = s;
+    visitados[s] = 1;
 
-    while (front < rear) {
-        int nextLevelSize = 0; // Number of elements in the next level
+    int apertos = 0;
+    int quantidade = 1;
 
-        for (int i = 0; i < levelSize; i++) {
-            int current_floor = queue[front++];
+    while (inicio < fim)
+    {
+        int quantidade_nova = 0;
 
-            if (current_floor == g) {
-                free(visited);
-                free(queue);
-                return pushes;
+        for (int i = 0; i < quantidade; i++)
+        {
+            int atual = fila[inicio++];
+
+            if (atual == g)
+            {
+                free(visitados);
+                free(fila);
+                return apertos;
             }
 
-            int up_floor = current_floor + u;
-            int down_floor = current_floor - d;
+            int andar_acima = atual + u;
+            int andar_abaixo = atual - d;
 
-            if (up_floor <= f && visited[up_floor] == 0) {
-                visited[up_floor] = 1;
-                queue[rear++] = up_floor;
-                nextLevelSize++;
+            if (andar_acima <= f && visitados[andar_acima] == 0)
+            {
+                visitados[andar_acima] = 1;
+                fila[fim++] = andar_acima;
+                quantidade_nova++;
             }
 
-            if (down_floor >= 1 && visited[down_floor] == 0) {
-                visited[down_floor] = 1;
-                queue[rear++] = down_floor;
-                nextLevelSize++;
+            if (andar_abaixo >= 1 && visitados[andar_abaixo] == 0)
+            {
+                visitados[andar_abaixo] = 1;
+                fila[fim++] = andar_abaixo;
+                quantidade_nova++;
             }
+
         }
-
-        pushes++; // Increment the pushes after processing the current level
-        levelSize = nextLevelSize; // Update the level size for the next level
+        apertos++;
+        quantidade = quantidade_nova;
     }
 
-    free(visited);
-    free(queue);
+    free(visitados);
+    free(fila);
 
-    return -1;  // "use the stairs" case
+    return -1;
 }
 
-int main() {
+int main()
+{
     int f, s, g, u, d;
+
     scanf("%d %d %d %d %d", &f, &s, &g, &u, &d);
 
-    int result = minButtonPresses(f, s, g, u, d);
+    int apertos = min_apertar(f, s, g, u, d);
 
-    if (result == -1) {
+    if(apertos  == -1)
         printf("use the stairs\n");
-    } else {
-        printf("%d\n", result);
-    }
+
+    else
+        printf("%d\n", apertos);
 
     return 0;
 }
