650d74a8183a8101d45bf43764dd9c034088fac8
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-11-06 16:36:24 -0300

    Exercício cartas

--- exercicios_moj/lista_2/cartas.c
+++ exercicios_moj/lista_2/cartas.c
@@ -1,89 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct celula
+void descarta_cartas(int n)
 {
-    int *dado;
-    int tamanho;
-    int inicio;
-    int fim;
-} celula;
+    int *cartas = malloc(sizeof(int) * n * 2);
+    int *cartas_descartadas = malloc(sizeof(int) * n);
+    int inicio = 0;
+    int fim = n - 1;
+    int fim_descarte = 0;
 
-void empilha(celula *lista, int numero)
-{
-    if (lista->fim == lista->tamanho)
+    for (int i = 0; i < n; i++)
+        cartas[i] = i + 1;
+
+    for (int i = 1; i < n; i++)
     {
-        lista->tamanho *= 2;
-        lista->dado = realloc(lista->dado, sizeof(int) * lista->tamanho);
+        cartas_descartadas[fim_descarte++] = cartas[inicio];
+        cartas[++fim] = cartas[inicio + 1];
+        inicio += 2;
     }
-
-    lista->dado[lista->fim] = numero;
-    lista->fim++;
-    lista->inicio++;
-}
-
-int desempilha(celula *lista)
-{
-    int salva = lista->dado[lista->inicio];
-
-    lista->inicio++;
-    lista->dado[lista->inicio];
-
-    return salva;
-}
-
-celula *cria_baralho(int numero)
-{
-    celula *lista = malloc(sizeof(celula));
-
-    lista->tamanho = numero;
-    lista->dado = malloc(sizeof(int) * numero);
-    lista->fim = 0;
-    lista->inicio = 0;
-
-    for (int i = 0; i < numero; i++)
-        lista->dado[i] = i + 1;
     
-    return lista;
+    printf("Cartas descartadas: %d", cartas_descartadas[0]);
+
+    for (int j = 1; j < fim_descarte; j++)
+        printf(", %d", cartas_descartadas[j]);
+
+    printf("\n");
+
+    printf("Carta restante: %d", cartas[inicio]);
+
+    printf("\n");
 }
 
-void descarta(celula *lista, int descarte[])
-{
-    for(int i = 0; i < lista->tamanho - 1; i++)
-    {
-        descarte[i] = desempilha(lista);
-        empilha(lista, lista->dado[lista->inicio]);
-    }
-}
 
-void imprime(celula *lista)
-{
-    for(int i = 0; i < lista->tamanho; i++)
-        printf("%d -> ", lista->dado[i]);
-
-    printf("NULL");
-}
-
-void imprime_vetor(int *lista, int tamanho)
-{
-    for(int i = 0; i < tamanho; i++)
-        printf("%d -> ", lista[i]);
-
-    printf("NULL\n");
-}
 
 int main()
 {
-    celula *lista = cria_baralho(7);
-    int *descarte = malloc(sizeof(int) * lista->tamanho);
+    int n;
+    scanf("%d", &n);
 
-    descarta(lista, descarte);
-
-    imprime_vetor(descarte, lista->tamanho);
-
-    imprime(lista);
+    descarta_cartas(n);
 
     return 0;
 }
-
-//fazer as operações do vetor (empilhando as cartas que não são descartadas) e armazenando as cartas descartadas (desempilhando o vetor) em um segundo vetor "descarte".
