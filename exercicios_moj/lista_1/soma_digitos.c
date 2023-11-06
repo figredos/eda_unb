c1083971498ca3f97ab3f20939b994ade6946715
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-11-06 17:13:40 -0300

    Exercício soma_digitos comentado

--- exercicios_moj/lista_1/soma_digitos.c
+++ exercicios_moj/lista_1/soma_digitos.c
@@ -1,13 +1,17 @@
+//Incluindo pacotes
 #include <stdio.h>
 #include <stdlib.h>
 
+//Função que soma os dígitos de um número recursivamente
 int soma_digitos(long long int numero, int soma)
 {
-    if (numero == 0)
+    if (numero == 0)    //Condição de parada
         return soma;
 
-    else
+    else    
     {
+        /*Subtraímos o valor do módulo do número por 10 do valor do número, em seguida atualizamos o valor do 
+        número com o valor do módulo do número por 10, finalmente dividimos esse valor por 10*/
         soma = soma + (numero % 10);
         numero = (numero - (numero % 10)) / 10;
         return soma_digitos(numero, soma);
