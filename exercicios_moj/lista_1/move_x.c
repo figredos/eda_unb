b0d8c1f475e79962de285a26b5d3ee7d5d47a0a4
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-11-06 17:13:20 -0300

    Exercício move_x comentado

--- exercicios_moj/lista_1/move_x.c
+++ exercicios_moj/lista_1/move_x.c
@@ -1,32 +1,34 @@
+//Incluindo pacotes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//Função que move todas as instâncias da letra "x" em uma string para o seu fim (recursiva)
 void move_x(char* string, int indice, int contador)
 {
     char temp;
 
     if (indice < strlen(string))
     {
-        if (string[indice] == 0)
+        if (string[indice] == 0)    //Se o indice da string for "\0"
             return;
 
-        else if (string[indice] != 120)
+        else if (string[indice] != 120) //Se o índice da string for diferente de "x", bota a letra no início
         {
-            temp = string[contador];
+            temp = string[contador];    
             string[contador] = string[indice];
             string[indice] = temp;
 
             move_x(string, indice + 1, contador + 1);
         }
-        else
+        else    //Se o índice for igual a "x", mantém sua posição
             move_x(string, indice + 1, contador);
     }
 }
 
 int main()
 {
-    char *str = malloc(sizeof(char) * 100);
+    char *str = malloc(sizeof(char) * 101);
 
     scanf("%s", str);
 
