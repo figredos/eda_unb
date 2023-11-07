6a464f227ad275ebb48b2a22e4563a505e74f691
Matheus Fellet <101260213+figredos@users.noreply.github.com>
2023-11-07 16:19:23 -0300

    Algoritmo de ordenação - Selection Sort

---
+++ ordenacao/selection_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void selection_sort(int *v, int l, int r)
+{
+    int menor;
+    for (int i = l; i < r; i++)
+    {
+        menor = i;
+        for (int j = i + 1; j < r; j++)
+            if (v[j] < v[menor])
+                menor = j;
+
+        if (menor != i)
+        {
+            int t = i;
+            i = menor;
+            menor = i;
+        }
+    }
+}
