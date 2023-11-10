4f6351ef0887c1c95471cbb59f0b3c8429e40c29
figredos <matheusmfellet@gmail.com>
2023-11-10 15:39:09 -0300

    Algoritmo shell sort

---
+++ ordenacao/sorts/shell_sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void shell_sort(int *v, int l, int r)
+{
+    int h = 1;
+
+    while (h < (r - l + 1)/3)
+        h = 3*h + 1;
+
+    while (h >= 1)
+        for (int i = l + 1; i < r; i++)
+        {
+            for (int j = i; j >= i + h && v[j] < v[j - h]; j -= h)
+            {
+                int t = v[j];
+                v[j] = v[j - h];
+                v[j - h] = t;
+            }
+            h /= 3;
+        }
+}
