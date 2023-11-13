36aee575a2736aafa47d1bcf94368272b7e4a76d
figredos <matheusmfellet@gmail.com>
2023-11-13 15:45:01 -0300

    Arquivo teste

--- teste.c
+++ teste.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef int Item;
-#define less(A,B) ((A) < (B))
-#define lesseq(A,B) ((A) <= (B))
-#define exch(A,B) { Item t; t=A;A=B;B=t; }
-#define cmpexch(A,B) { if (less(B,A)) exch(A,B); }
 
-// static void insertionsortH(Item *v,int l,int r,int h)
-// {
-//   for(int i=l+h;i<=r;i++)
-//   {
-//     int j=i; Item tmp=v[j];
-//     while(j>=l+h && less(tmp,v[j-h]))
-//     {
-//       v[j]=v[j-h];
-//       j-=h;
-//     }
-//     v[j]=tmp;
-//   }
-// }
-
-// void shellsort(Item *v,int l,int r)
-// {
-//   int h;
-//   int t=(r-l)+1;
-//   for(h=1;h<=(t-1)/9;h=3*h+1);
-//   for(;h>0;h/=3)
-//     insertionsortH(v,0,t-1,h);
-// }
+void shellsort(int *v, int l, int r)
+{
+    int h;
+    for (h = 1; h <= (r - l) / 9; h = 3 * h + 1)
+        ;
+    for (; h > 0; h /= 3)
+        for (int i = l + h; i <= (r - l); i++)
+        {
+            int j = i;
+            int tmp = v[j];
+            while (j >= l + h && tmp < v[j - h])
+            {
+                v[j] = v[j - h];
+                j -= h;
+            }
+            v[j] = tmp;
+        }
+}
 
 void shell_sort(int *v, int l, int r)
 {
     int h;
+    for (h = 1; h <= (r - l) / 9; h = 3 * h + 1)
+        ;
 
-    for (h = 1; h <= (r - l) / 9; h = 3 * h + 1);
-
-    for (; h > 0; h / 3)
-        for (int i = l + h; i < r; i++)
+    for (; h > 0; h /= 3)
+        for (int i = l + h; i <= (r - l); i++)
         {
-            for (int j = i; j > l + h && v[j] < v[j - h]; j -= h)
+            int j = i;
+            int tmp = v[j];
+            while (j >= l + h && tmp < v[j - h])
             {
-                int t = v[j];
                 v[j] = v[j - h];
-                v[j - h] = t;
+                j -= h;
             }
+            v[j] = tmp;
         }
 }
 
-
 int main()
 {
-    int *vetor = malloc(50001 * sizeof(int));
+    // int *vetor = malloc(50001 * sizeof(int));
+    int vetor[] = {7, 3, 2, 5, 4, 3};
     int numero;
-    int i;
+    // int i;
+    int i = 5;
 
-    for (i = 0; scanf("%d", &numero) != EOF; i++)
-        vetor[i] = numero;
+    // for (i = 0; scanf("%d", &numero) != EOF; i++)
+    //     vetor[i] = numero;
 
     shell_sort(vetor, 0, i);
 
-    printf("%d", vetor[1]);
- 
-    for (int j = 2; j <= i; j++)
+    printf("%d", vetor[0]);
+
+    for (int j = 1; j <= i; j++)
         printf(" %d", vetor[j]);
 
     printf("\n");
