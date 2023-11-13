6a4882353520a44c38071a9c84447700a4eeeeea
figredos <matheusmfellet@gmail.com>
2023-11-13 15:44:50 -0300

    Exercício Insertion Sort

--- exercicios_moj/lista_3/ordena_insercao.c
+++ exercicios_moj/lista_3/ordena_insercao.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// void insertion_sort(int *v, int l, int r)
-// {
-//     for (int i = l + 1; i < r; i++)
-//     {
-//         for (int j = i; j > l && v[j] < v[j - 1]; j--)
-//         {
-//             int t = v[j];
-//             v[j] = v[j - 1];
-//             v[j - 1] = t;
-//         }
-//     }
-// }
-
 void shell_sort(int *v, int l, int r)
 {
     int h = 1;
 
-    for (; h <= (r - l)/9; h = 3*h + 1)
+    for (; h <= (r - l) / 9; h = 3 * h + 1)
         ;
-    
-    for (; h > 0;  h /= 3)
-        for (int i = h + l; i <= (r - l); i++)
+
+    for (; h > 0; h /= 3)
+        for (int i = l + h; i <= (r - l); i++)
             for (int j = i; j >= l + h && v[j] < v[j - h]; j -= h)
             {
                 int t = v[j];
                 v[j] = v[j - h];
-                v[j - h] = t;   
-            }    
+                v[j - h] = t;
+            }
 }
 
 int main()
